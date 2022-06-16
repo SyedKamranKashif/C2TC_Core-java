255fe888c121760fb63ae52a7fb774d267958513
admin <admin@kamr>
2022-06-16 16:03:06 +0530

    first commit

---
+++ Access Modifiers/src/AccessModifiersDemo/test2.java
@@ -0,0 +1,12 @@
+package AccessModifiersDemo;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		 Test1 t1=new Test1();
+		 t1.show();
+		 System.out.println(t1.i);
+
+	}
+
+}
---
+++ Access Modifiers/src/demo1/test3.java
@@ -0,0 +1,11 @@
+package demo1;
+ import AccessModifiersDemo.Test1;
+
+public class test3 extends Test1{
+
+	public static void main(String[] args) {
+		test3 t3=new test3();
+		t3.show();
+		System.out.println(t3.i);
+	}
+}
