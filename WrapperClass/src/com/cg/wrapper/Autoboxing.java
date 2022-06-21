e1262a3900fc84c155630718569b11236f43288d
admin <admin@kamr>
2022-06-21 16:08:35 +0530

    first commit

---
+++ WrapperClass/src/com/cg/wrapper/Autoboxing.java
@@ -0,0 +1,18 @@
+package com.cg.wrapper;
+
+public class Autoboxing {
+
+	public Autoboxing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int a=20;
+		Integer i= Integer.valueOf(a);
+		Integer j=a;
+		System.out.println(a+ " " +i + " " +j);
+		// TODO Auto-generated method stub
+
+	}
+
+}
